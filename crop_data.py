1fab5eb49793a39ffe44ce18d1e4bb91f3143016
Caio Passos T. Ferreira <119456930+stepsbtw@users.noreply.github.com>
2023-10-16 17:31:56 -0300

    Update crop_data.py

--- crop_data.py
+++ crop_data.py
@@ -2,31 +2,35 @@ import os
 import numpy
 from PIL import Image
 
-
 DATA_BANK = Image.open(os.path.join('data.png'))
 
 def main():
-    inputs = crop_data(DATA_BANK,28,28,100,10)
-    # aplicar hash pra facilitar o reconhecimento
-    hash = {}
-    for i,input in 
-    return
+   #crop_data(DATA_BANK, 28, 28, 100, 10)
+   # aplicar hash pra facilitar o reconhecimento
+   inputs = []
+   input_list = []
+   for i in range(100):
+      inputs.append(Image.open(os.path.join('data_input/'f'input{i+1}.png')))
+      
+   for i in range(0,100,10):
+      input_list.append(inputs[i:i+90:10])
 
-def crop_data(data_bank,width,height,qt,columns):
-    posx = 0
-    posy = 0
-    data = []
-    for i in range(qt):
-        img_rect = (posx,posy,posx+width,posy+height)
-        img = data_bank.crop(img_rect)
-        data.append(img)
-        data[i].save('data_input/'f'input{i}.png')
-        posx += width+1 # linhas que dividem os dados
-        if i % columns == 0 and i > 0:
-            posx = 0
-            posy+=height+1
+   print(input_list[0])
+   return
 
-    return data
 
-        
+def crop_data(data_bank, width, height, qt, columns):
+   posx = 0
+   posy = 0
+   for i in range(1, qt + 1):
+      img_rect = (posx, posy, posx + width, posy + height)
+      img = data_bank.crop(img_rect)
+      img.save('data_input/'
+               f'input{i}.png')
+      posx += width + 1  # linhas que dividem os dados
+      if i % columns == 0 and i > 0:
+         posx = 0
+         posy += height + 1
+
+
 main()
