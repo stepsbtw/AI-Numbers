6553fd3b43695fad2b875e2d22c61331d9d5259b
Caio Passos T. Ferreira <119456930+stepsbtw@users.noreply.github.com>
2023-10-16 13:36:52 -0300

    Update crop_data.py

--- crop_data.py
+++ crop_data.py
@@ -2,20 +2,28 @@ import os
 import numpy
 from PIL import Image
 
+
+DATA_BANK = Image.open(os.path.join('data.png'))
+
 def main():
-    data_bank = Image.open(os.path.join('data.png'))
+    inputs = crop_data(DATA_BANK,28,28,100,10)
+    return
+
+def crop_data(data_bank,width,height,qt,columns):
     posx = 0
     posy = 0
     data = []
-    for i in range(100): # cortando os 100 inputs.
-        img_rect = (posx,posy,posx+28,posy+28)
+    for i in range(qt):
+        img_rect = (posx,posy,posx+width,posy+height)
         img = data_bank.crop(img_rect)
         data.append(img)
         data[i].save('data_input/'f'input{i}.png')
-        posx += 29 # adicionando 29 pq tem uma linha de 1 pixel entre as imagens.
-        if i % 10 == 0 and i > 0:
+        posx += width+1 # linhas que dividem os dados
+        if i % columns == 0 and i > 0:
             posx = 0
-            posy+=29
-        
+            posy+=height+1
 
+    return data
+
+        
 main()
