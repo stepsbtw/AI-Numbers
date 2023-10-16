0fe7bf8818eea326c212207a8eb1f5f80282289b
stepsbtw <stepsbtw@gmail.com>
2023-10-15 23:33:50 -0300

    updates

---
+++ Main.py
@@ -0,0 +1,30 @@
+import random
+import os
+import numpy
+
+
+data_bank = [os.path.join('data.png')]
+
+for i in range(20):
+    weights.append(random.randint(-1000,1000))
+
+class AI:
+    def __init__(self,weights):
+        self.weights = weights
+
+    def brain(self,inputs):
+        outputs = [0,0,0,0,0,0,0,0,0,0]
+        outputs[0] = x
+        outputs[1] = x
+        outputs[2] = x
+        outputs[3] = x
+        outputs[4] = x
+        outputs[5] = x
+        outputs[6] = x
+        outputs[7] = x
+        outputs[8] = x
+        outputs[9] = x
+
+
+def main():
+    return
