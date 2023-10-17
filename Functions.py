5e70ba01af570d6bd4810222f6872cf0ea10c6dc
stepsbtw <stepsbtw@gmail.com>
2023-10-17 13:41:48 -0300

    updates

---
+++ Functions.py
@@ -0,0 +1,4 @@
+import numpy
+
+def sigmoid(x):
+    return 1/(1 + numpy.exp(-x))
--- Main.py
+++ Main.py
@@ -1,17 +1,38 @@
 import numpy
+import math
 import crop_data
 from PIL import Image
 import random
+import Functions
 
 images_list = crop_data.images_list()
-input_image = images_list[0] # por enquanto
+input_image = images_list[0][0] # por enquanto
 
+def main():
+    ia = Ia()
+    ia.brain(input_image)
 
 class Ia:
     def __init__(self):
         self.weights = []
-    
+        self.bias = []
+        for i in range(7840): # inputs * outputs
+            self.weights.append(random.uniform(-1,1))
+        for i in range(10):
+            self.bias.append(random.randint(-10000000,10000000))
+        
     def brain(self,input_image):
-        self
+        input_image = input_image.convert('L') # GREYSCALE
+        inputs = numpy.array(input_image) # pixels
+        outputs = []
+        for i in range(10):
+            output_sum = 0 # so pra poder somar certinho
+            for j in range(1,28):
+                for k in range(1,28):
+                    weight_index = ((i+1) * (k*j))-1 # index do output +1 * index da linha * index do elemento tudo -1 pq index.
+                    output_sum += (inputs[j][k] * self.weights[weight_index])
+            
+            outputs.append(numpy.tanh((output_sum + self.bias[i])/10000000)) # preciso dos BIAS.
+            print(outputs)
 
 main()
