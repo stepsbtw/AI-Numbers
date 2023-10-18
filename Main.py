1cc3bc8b8e033405db2af487ff897c59c3eeff13
Caio Passos T. Ferreira <119456930+stepsbtw@users.noreply.github.com>
2023-10-17 22:43:02 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,28 +9,29 @@ images_list = crop_data.images_list()
 input_image = images_list[0][0] # por enquanto
 
 def main():
+    ias = []
     for i in range(10):
-        ias = Ia()
-        print(ias)
+        ias.append(Ia())
+    check(ias,images_list,input_image)
 
-def check(ias):
-    answer = images_list.index(input_image)
+def check(ias,images_list,input_image):
+    answer = images_list[0].index(input_image)
+    print(answer)
     ias_answer = []
     outputs = []
     best = ias[0]
-    best_outputs = best.brain()
+    best_outputs = best.brain(input_image)
     best_answer = best_outputs[answer]
     for i in range(1,len(ias)):
-        outputs = (ias[i].brain())
+        outputs = (ias[i].brain(input_image))
+        print(f"sou a ia {i}")
         ias_answer.append(outputs[answer])
-        if ias_answer[i] > best_answer:
+        if ias_answer[i-1] > best_answer:
             best = ias[i]
-            best_outputs = ias_answer[i]
-            best_answer = best_outputs[answer]
-     print(best)
-        
-    
-    
+            best_index = i
+            best_answer = ias_answer[i-1]
+    print(f"a melhor eh a ia {best_index}")
+    print(f"{best_answer} de chance de ser 0!")
     
     
 
@@ -55,12 +56,9 @@ class Ia:
                     weight_index = ((i+1) * (k+1*j+1))-1 # index do output +1 * index da linha * index do elemento tudo -1 pq index.
                     output_sum += ((inputs[j][k]/255) * self.weights[weight_index])
             outputs.append(Functions.sigmoid((output_sum + self.biases[i])/100)) # preciso dos BIAS.
-        print(outputs)
-        self.activation(outputs)
-
-    def activation(self,outputs):
-        for i in range(len(outputs)):
-            print(f"acho que {outputs[i]} {i}")
+        #for i in range(len(outputs)):
+            #print(f"acho que {outputs[i]} {i}")
+        print(f'acho que {outputs[0]} 0')
         return outputs
 
 main()
